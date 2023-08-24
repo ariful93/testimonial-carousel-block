5582530aa457f543169d88e19e9943f422322a92
Ariful Islam <arif13414@gmail.com>
2023-08-24 10:41:53 +0600

    Fix spacing issue of php file

--- testimonial-carousel-gutenberg-block.php
+++ testimonial-carousel-gutenberg-block.php
@@ -17,100 +17,101 @@
 
 /**
  * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
+ * Behind the scenes, it registers also all assets, so they can be enqueued
  * through the block editor in the corresponding context.
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
 
-if (!defined('ABSPATH')) {
-	exit;
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
 }
 
 /**
- * Testimonial_Carousel_Gutenberg_Block class
+ * Testimonial_Carousel_Gutenberg_Block class.
  *
- * @class Testimonial_Carousel_Gutenberg_Block The class that holds the entire Testimonial_Carousel_Gutenberg_Block plugin
+ * @class Testimonial_Carousel_Gutenberg_Block The class that holds the entire Testimonial_Carousel_Gutenberg_Block plugin.
  */
 class Testimonial_Carousel_Gutenberg_Block {
-	/**
-   * Singleton pattern
-   *
-   * @var bool $instance
-   */
-  private static $instance = false;
 
-  /**
-   * Initializes the Testimonial_Carousel_Gutenberg_Block class
-   *
-   * Checks for an existing Testimonial_Carousel_Gutenberg_Block instance
-   * and if it cant't find one, then creates it.
-   */
-  public static function init()
-  {
+    /**
+     * Singleton pattern
+     *
+     * @var bool $instance
+     */
+    private static $instance = false;
 
-    if (!self::$instance) {
-      self::$instance = new self();
+    /**
+     * Initializes the Testimonial_Carousel_Gutenberg_Block class.
+     *
+     * Checks for an existing Testimonial_Carousel_Gutenberg_Block instance
+     * and if it can't find one, then creates it.
+     *
+     * @since 1.0
+     *
+     * @return self
+     */
+    public static function init() {
+        if ( ! self::$instance ) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
     }
 
-    return self::$instance;
-  }
+    /**
+     * Constructor for the Testimonial_Carousel_Gutenberg_Block class
+     *
+     * Sets up all the appropriate hooks and actions
+     * within our plugin.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->includes(); // includes
+    }
 
-  /**
-   * Constructor for the Testimonial_Carousel_Gutenberg_Block class
-   *
-   * Sets up all the appropriate hooks and actions
-   * within our plugin.
-   *
-   * @return void
-   */
-	public function __construct()
-	{
+    /**
+     * Load all includes file
+     *
+     * @since 0.0.1
+     *
+     * @return void
+     */
+    public function includes() {
+        add_action( 'init', array( $this, 'testimonial_carousel_block_register_init' ) );
+    }
 
-		// includes
-		$this->includes();
-		
-	}
+    /**
+     * Register gutenberg blocks.
+     *
+     * @since 1.0
+     *
+     * @return void
+     */
+    public function testimonial_carousel_sub_block_register( $name, $options = array() ) {
+        register_block_type( __DIR__ . '/build/' . $name, $options );
+    }
 
-	/**
-	 * Load all includes file
-	 *
-	 * @since 0.0.1
-	 *
-	 * @return void
-	 */
-	public function includes() {
-    	add_action('init', array($this, 'testimonial_carousel_block_register_init'));  
-
-  	}
-
-	/**
-	 * Register blocks
-	 */
-	public function testimonial_carousel_sub_block_register( $name, $options = array() ) {
-		register_block_type( __DIR__ . '/build/' . $name, $options );
-
-	}
-
-	/**
-	 * Blocks initialize
-	 */
-	public function testimonial_carousel_block_register_init() {
-		$this->testimonial_carousel_sub_block_register( 'testimonial-carousel' );
-
-	}
-
+    /**
+     * Initialize blocks registration.
+     *
+     * @since 1.0
+     *
+     * @return void
+     */
+    public function testimonial_carousel_block_register_init() {
+        $this->testimonial_carousel_sub_block_register( 'testimonial-carousel' );
+    }
 }
 
 /**
- * Init the team member for gutenberg plugin
+ * Init the team member for gutenberg plugin.
  *
  * @return Testimonial_Carousel_Gutenberg_Block the plugin object
  */
-function testimonial_carousel_gutenberg_block()
-{
-
-  return Testimonial_Carousel_Gutenberg_Block::init();
+function testimonial_carousel_gutenberg_block() {
+    return Testimonial_Carousel_Gutenberg_Block::init();
 }
 
 testimonial_carousel_gutenberg_block();
